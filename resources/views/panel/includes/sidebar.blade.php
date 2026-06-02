0b20a4e213d9d981c5d10f6af4d3c41050f3ff1c
Laxmi Shaha <laxmishaha028@gmail.com>
2026-06-02 13:09:40 +0600

    Saving progress before switching branches

---
+++ resources/views/panel/includes/sidebar.blade.php
@@ -0,0 +1,89 @@
+     <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-graduation-cap"></i> <span>Alumni Management System</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Dashboard <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="index.html">Admin Dashboard</a></li>
+                      <li><a href="index2.html">Alumni Dashboard</a></li>
+                      <li><a href="index3.html">Accounts Dashboard</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Events <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Event Create</a></li>
+                      <li><a href="form_advanced.html">Event Edit </a></li>
+                      <li><a href="form_validation.html">Event Update</a></li>
+                      <li><a href="form_wizards.html">Event delete</a></li>
+                      <li><a href="form_upload.html">Event List</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-desktop"></i> Donation <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="general_elements.html">Donation Category</a></li>
+                      <li><a href="media_gallery.html">Donor List</a></li>
+                      <li><a href="typography.html">Donor Add</a></li>
+                      <li><a href="icons.html">Donation report</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-table"></i> Accounts <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="tables.html">Income</a></li>
+                      <li><a href="tables_dynamic.html">Expense</a></li>
+                           <li><a href="tables_dynamic.html">Revenue</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-bar-chart-o"></i> Directory <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="chartjs.html">Browse Alumni</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-clone"></i>commit <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="fixed_sidebar.html">President</a></li>
+                      <li><a href="fixed_footer.html">Treasurer</a></li>
+                      <li><a href="fixed_footer.html">Member</a></li>
+                    </ul>
+                  </li>
+                       </li>
+                  <li><a><i class="fa fa-clone"></i> Basic Setup <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="fixed_sidebar.html"> Role setup </a></li>
+                      <li><a href="fixed_footer.html"> Committee Setup </a></li>
+                      <li><a href="fixed_footer.html"> Category Setup </a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+                     </div>
+            <div class="sidebar-footer hidden-small">
+              <a data-toggle="tooltip" data-placement="top" title="Settings">
+                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Lock">
+                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+              </a>
+            </div>
+             </div>
+        </div>
